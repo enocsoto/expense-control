008d1440a24baefd8446e13f018c22dcc032e79c
Enoc de Jesus Soto Arenilla <tec-enocdejesus@hotmail.com>
2025-08-14 12:04:35 -0300

    add localstorage and functions budget and expense initial states

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import './App.css';
 import { useBudget } from './hooks/useBudget';
 import BudgetTracker from './components/BudgetTracker';
@@ -9,6 +9,11 @@ import ExpenseList from './components/ExpenseList';
 function App() {
   const { state } = useBudget();
   const isValidBudget = useMemo(() => state.budget > 0, [state.budget]);
+
+  useEffect(() => {
+    localStorage.setItem('budget', state.budget.toString());
+    localStorage.setItem('expenses', JSON.stringify(state.expenses));
+  }, [state]);
   return (
     <>
       <header className="bg-blue-600 py-8 max-h-72">
